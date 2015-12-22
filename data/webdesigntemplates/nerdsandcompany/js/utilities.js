4af580e0c41d76e11466f426573dd2801d71f6b4
Wouter Hendriks <w.hendriks@nerds.company>
2015-12-22 12:26:58 +0100

    Renamed 'support.js' to 'utilities.js'

--- data/webdesigntemplates/nerdsandcompany/js/utilities.js
+++ data/webdesigntemplates/nerdsandcompany/js/utilities.js
@@ -1,19 +1,24 @@
 "use strict";
 
-module.exports.getSiteConfig = function()
-{
+module.exports.getSiteConfig = function() {
+
   var whconfigel = document.querySelector('script#wh-config');
 
   // Make sure we have obj/site as some sort of object, to prevent crashes on naive 'if ($wh.config.obj.x)' tests'
   var siteconfig = whconfigel ? JSON.parse(whconfigel.textContent) : {};
 
   if (!siteconfig.obj) {
+
     siteconfig.obj = {};
+
   }
-  
+
   if (!siteconfig.site) {
+
     siteconfig.site = {};
+
   }
 
   return siteconfig;
+
 };
